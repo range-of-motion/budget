a41dd18e0616383894e805e677dabc3730f2b8f7
Daniël van Drunen <daniel@pixely.me>
2017-07-20 19:47:48 +0200

    Add LogoutController

---
+++ app/Http/Controllers/LogoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutController extends Controller {
+    public function index() {
+        Auth::logout();
+
+        return redirect()->route('login_get');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/login', 'LoginController@index')->name('login_get');
 Route::post('/login', 'LoginController@store')->name('login_post');
 
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard_get');
+
+Route::get('/logout', 'LogoutController@index')->name('logout_get');
