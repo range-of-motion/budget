0e7f10cd83ac70a543600b58340e697f5f211f57
Daniël van Drunen <daniel@pixely.me>
2017-07-20 23:02:47 +0200

    Add stylesheet

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,21 @@
-Hello
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Budget</title>
+        <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+        <div class="navigation">
+            <div class="navigation__wrapper">
+                <ul>
+                    <li>
+                        <a href="/dashboard">Dashboard</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="body__wrapper">
+            Hello
+        </div>
+    </body>
+</html>
