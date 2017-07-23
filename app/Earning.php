ce396e49d9a491373bfa551228d493b2a6e6e060
Daniël van Drunen <daniel@pixely.me>
2017-07-23 13:25:45 +0200

    Create model/migration for earnings

---
+++ app/Earning.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Earning extends Model {
+    //
+}
---
+++ database/migrations/2017_07_23_112354_create_earnings_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEarningsTable extends Migration {
+    public function up()
+    {
+        Schema::create('earnings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->date('date');
+            $table->string('description');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('earnings');
+    }
+}
