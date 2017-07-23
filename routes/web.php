21828f258b2112c8a57d3cf3bbdf38bbcf8012b8
Daniël van Drunen <daniel@pixely.me>
2017-07-23 13:34:12 +0200

    Protect certain routes with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 Route::get('/login', 'LoginController@index')->name('login_get');
 Route::post('/login', 'LoginController@store')->name('login_post');
 
-Route::get('/dashboard', 'DashboardController@index')->name('dashboard_get');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard_get');
+});
 
 Route::get('/logout', 'LogoutController@index')->name('logout_get');
