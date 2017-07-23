1383ca25efafe3dc0e54a1c70ecf45c2c7acbc30
Daniël van Drunen <daniel@pixely.me>
2017-07-23 15:23:31 +0200

    Add log out to navigation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,6 +15,9 @@
                     <li>
                         <a href="/settings">Settings</a>
                     </li>
+                    <li>
+                        <a href="/logout">Log out</a>
+                    </li>
                 </ul>
             </div>
         </div>
