d576d389a1b9803a7b387a786c36207500cf5cb3
Daniël van Drunen <daniel@pixely.me>
2017-08-03 19:54:47 +0200

    Create model/migration for budgets

---
+++ database/migrations/2017_08_03_175227_create_budgets_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBudgetsTable extends Migration {
+    public function up() {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('month');
+            $table->integer('year');
+            $table->float('amount');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('budgets');
+    }
+}
