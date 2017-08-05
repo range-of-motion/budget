00c82eadbc692e1d1a1e89d7403d799c438136f4
Daniël van Drunen <daniel@pixely.me>
2017-08-05 21:44:24 +0200

    Display budgets on reports's show view

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -4,6 +4,16 @@
     <h1>Report for @lang('months.' . $month), {{ $year }}</h1>
     <div class="box spacing-bottom-large">
         <div class="box__section">
-            <p>Placeholder</p>
+            <p>Budgets</p>
         </div>
+        <table class="box__section">
+            <tbody>
+                @foreach (App\Budget::where('user_id', Auth::user()->id)->whereMonth('date', $month)->whereYear('date', $year)->get() as $budget)
+                    <tr>
+                        <td>{{ $budget->tag->name }}</td>
+                        <td>{{ Auth::user()->currency->symbol }} {{ App\Spending::where('user_id', Auth::user()->id)->whereMonth('date', $month)->whereYear('date', $year)->where('tag_id', $budget->tag->id)->sum('amount') }} of {{ Auth::user()->currency->symbol }} {{ $budget->amount }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
 @endsection
