e6cae6d76f531cde0a5d7cc557274be8dc6bde63
Daniël van Drunen <daniel@pixely.me>
2017-08-06 16:09:39 +0200

    Display alerts on reports's show view

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -4,6 +4,22 @@
     <h1>Report for @lang('months.' . $month), {{ $year }}</h1>
     <div class="box spacing-bottom-large">
         <div class="box__section">
+            <p>Alerts</p>
+        </div>
+        <table class="box__section">
+            <tbody>
+                @foreach (App\Budget::where('user_id', Auth::user()->id)->whereMonth('date', $month)->whereYear('date', $year)->get() as $budget)
+                    @if (App\Spending::where('user_id', Auth::user()->id)->whereMonth('date', $month)->whereYear('date', $year)->where('tag_id', $budget->tag->id)->sum('amount') > $budget->amount)
+                        <tr>
+                            <td>Exceeded budget for '{{ $budget->tag->name }}' by @include('partials.currency') {{ App\Spending::where('user_id', Auth::user()->id)->whereMonth('date', $month)->whereYear('date', $year)->where('tag_id', $budget->tag->id)->sum('amount') - $budget->amount }}</td>
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="box spacing-bottom-large">
+        <div class="box__section">
             <p>Budgets</p>
         </div>
         <table class="box__section">
@@ -16,4 +32,5 @@
                 @endforeach
             </tbody>
         </table>
+    </div>
 @endsection
