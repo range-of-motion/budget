0db920eb08eaa03a97cc55ea70ddf8c51ff2aa09
Daniël van Drunen <daniel@pixely.me>
2017-09-05 17:33:33 +0200

    Add styling modifiers to row elements

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,15 +1,19 @@
 @extends('layout')
 
 @section('body')
-    <h1 class="spacing-bottom-large">Log in</h1>
-    <div class="box spacing-small">
-        <form method="POST">
-            {{ csrf_field() }}
-            <label>E-mail</label>
-            <input type="email" name="email" />
-            <label>Password</label>
-            <input type="password" name="password" />
-            <input type="submit" value="Log in" />
-        </form>
+    <div class="row">
+        <div class="column size-560 align-center">
+            <h1 class="spacing-bottom-large">Log in</h1>
+            <div class="box spacing-small">
+                <form method="POST">
+                    {{ csrf_field() }}
+                    <label>E-mail</label>
+                    <input type="email" name="email" />
+                    <label>Password</label>
+                    <input type="password" name="password" />
+                    <input type="submit" value="Log in" />
+                </form>
+            </div>
+        </div>
     </div>
 @endsection
