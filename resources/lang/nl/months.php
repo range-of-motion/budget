ff37e154ede3a0fcb9bb946eeec2427de4ee94fb
Daniël van Drunen <daniel@pixely.me>
2017-09-17 23:58:02 +0200

    Add Dutch translation for months

---
+++ resources/lang/nl/months.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    '1' => 'Januari',
+    '2' => 'Februari',
+    '3' => 'Maart',
+    '4' => 'April',
+    '5' => 'Mei',
+    '6' => 'Juni',
+    '7' => 'Juli',
+    '8' => 'Augustus',
+    '9' => 'September',
+    '10' => 'Oktober',
+    '11' => 'November',
+    '12' => 'December'
+];
