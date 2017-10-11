12d729c7e159fbac5d3ed065365232259a4c1659
Daniël van Drunen <daniel@pixely.me>
2017-10-11 14:21:01 +0200

    Add CurrenciesTableSeeder

---
+++ database/seeds/CurrenciesTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Currency;
+
+class CurrenciesTableSeeder extends Seeder {
+    public function run() {
+        Currency::create([
+            'name' => 'Euro',
+            'symbol' => '&eur;'
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+class DatabaseSeeder extends Seeder {
+    public function run() {
+        $this->call(CurrenciesTableSeeder::class);
     }
 }
