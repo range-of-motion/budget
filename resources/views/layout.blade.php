2d02c3ed7e45ef4a6d8ff34b2c86ca0616f872e1
Daniël van Drunen <daniel@pixely.me>
2017-11-02 16:21:24 +0100

    Clean up default Laravel Mix setup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i" />
         <link rel="stylesheet" href="/style.css" />
+        <link rel="stylesheet" href="/css/app.css" />
     </head>
     <body>
         @if (Auth::check())
