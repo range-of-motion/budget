d15fcc3b7eb347df9e363d1ec08e58c69a5310ea
Daniël van Drunen <daniel@pixely.me>
2017-11-04 17:59:36 +0100

    Add missing user_id values to TagsTableSeeder

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -8,12 +8,15 @@ class TagsTableSeeder extends Seeder {
     public function run() {
         Tag::insert([
             [
+                'user_id' => 1,
                 'name' => 'Groceries'
             ],
             [
+                'user_id' => 1,
                 'name' => 'Bills'
             ],
             [
+                'user_id' => 1,
                 'name' => 'Gas & Fuel'
             ]
         ]);
