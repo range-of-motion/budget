ae81ee1de40935a2951ed9aea980621e1950f85c
Daniël van Drunen <daniel@pixely.me>
2017-11-08 23:14:58 +0100

    Update routes (and the names thereof)

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,31 @@
 <?php
 
-Route::get('/login', 'LoginController@index')->name('login.index');
-Route::post('/login', 'LoginController@store')->name('login.store');
+Route::get('/login', 'LoginController@index')->name('login');
+Route::post('/login', 'LoginController@store');
 
-Route::get('/register', 'RegisterController@index');
+Route::get('/register', 'RegisterController@index')->name('register');
 Route::post('/register', 'RegisterController@store');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/dashboard/{year?}/{month?}', 'DashboardController@index')->name('dashboard.index');
+    Route::get('/dashboard/{year?}/{month?}', 'DashboardController@index')->name('dashboard');
 
-    Route::get('/search', 'SearchController@index');
+    Route::get('/search', 'SearchController@index')->name('search');
 
-    Route::resource('/earnings', 'EarningsController', ['only' => ['create', 'store']]);
+    Route::get('/earnings/create', 'EarningsController@create')->name('earnings.create');
+    Route::post('/earnings', 'EarningsController@store');
 
-    Route::resource('/spendings', 'SpendingsController', ['only' => ['create', 'store']]);
+    Route::get('/spendings/create', 'SpendingsController@create')->name('spendings.create');
+    Route::post('/spendings', 'SpendingsController@store');
 
-    Route::resource('/budgets', 'BudgetsController', ['only' => ['create', 'store']]);
+    Route::get('/budgets/create', 'BudgetsController@create')->name('budgets.create');
+    Route::post('/budgets', 'BudgetsController@store');
 
-    Route::resource('/tags', 'TagsController', ['only' => ['index', 'create', 'store']]);
+    Route::get('/tags', 'TagsController@index')->name('tags');
+    Route::get('/tags/create', 'TagsController@create')->name('tags.create');
+    Route::post('/tags', 'TagsController@store');
 
-    Route::get('/settings', 'SettingsController@index')->name('settings.index');
-    Route::post('/settings', 'SettingsController@store')->name('settings.store');
+    Route::get('/settings', 'SettingsController@index')->name('settings');
+    Route::post('/settings', 'SettingsController@store');
 });
 
-Route::get('/logout', 'LogoutController@index')->name('logout.index');
+Route::get('/logout', 'LogoutController@index')->name('logout');
