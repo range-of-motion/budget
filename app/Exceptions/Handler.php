c8a1bea47b5b6fa124610b453ecf551639e1754e
Daniël van Drunen <daniel@pixely.me>
2017-11-09 20:14:59 +0100

    Fix unauthorized redirect

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -60,6 +60,6 @@ class Handler extends ExceptionHandler
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
 
-        return redirect()->guest(route('login_get'));
+        return redirect()->guest(route('login'));
     }
 }
