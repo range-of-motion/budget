264ab102c80b048c28ec47d7186028686542ea89
Daniël van Drunen <daniel@pixely.me>
2017-11-16 16:32:17 +0100

    Add narrow modifier to wrapper SASS element

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('body')
-    <div class="wrapper spacing-top-large spacing-bottom-large">
+    <div class="wrapper narrow spacing-top-large spacing-bottom-large">
         <div class="box">
             <div class="section">
                 <h3>Log in</h3>
