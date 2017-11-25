246668eab4e1a6c922781f02bca5962fd60de6b9
Daniël van Drunen <daniel@pixely.me>
2017-11-25 17:14:25 +0100

    Fix empty void when user has no tags for tags' index view

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -13,20 +13,24 @@
                     </div>
                 </div>
             </div>
-            <ul class="section">
-                @foreach ($tags as $tag)
-                    <li class="row">
-                        <div class="column">{{ $tag->name }}</div>
-                        <div class="column">
-                            <form class="action" method="POST" action="/tags/{{ $tag->id }}">
-                                {{ method_field('delete') }}
-                                {{ csrf_field() }}
-                                <button>@lang('actions.delete')</button>
-                            </form>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
+            @if (count($tags) > 0)
+                <ul class="section">
+                    @foreach ($tags as $tag)
+                        <li class="row">
+                            <div class="column">{{ $tag->name }}</div>
+                            <div class="column">
+                                <form class="action" method="POST" action="/tags/{{ $tag->id }}">
+                                    {{ method_field('delete') }}
+                                    {{ csrf_field() }}
+                                    <button>@lang('actions.delete')</button>
+                                </form>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <div class="section">You don't have any tags</div>
+            @endif
         </div>
     </div>
 @endsection
