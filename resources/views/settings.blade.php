68e798d3aa47bc47c1ff0ff0f010c2ab5a14a2b7
Daniël van Drunen <daniel@pixely.me>
2018-08-13 17:29:20 +0200

    Display subtle message when you haven't created any tags yet

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -4,45 +4,43 @@
 
 @section('body')
     <div class="wrapper spacing-top-large spacing-bottom-large">
-        <div class="row row--gutter">
-            <div class="row__column">
-                <div class="spacing-bottom-large color-dark">Account</div>
-                <div class="box">
-                    <div class="section">
-                        <form method="POST">
-                            {{ csrf_field() }}
-                            <label>Name</label>
-                            <input type="text" name="name" value="{{ Auth::user()->name }}" />
-                            <label>E-mail</label>
-                            <input type="text" name="email" value="{{ Auth::user()->email }}" />
-                            <label>Language</label>
-                            <select name="language">
-                                @foreach ($languages as $language)
-                                    <option value="{{ $language }}" @if (Auth::user()->language === $language) selected @endif>{{ $language }}</option>
-                                @endforeach
-                            </select>
-                            <button>Update</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="row__column">
-                <div class="spacing-bottom-large color-dark">Tags</div>
-                <div class="box">
-                    <ul class="box__section">
-                        @foreach ($tags as $tag)
-                            <li class="row">
-                                <div class="row__column">{{ $tag->name }}</div>
-                                <div class="row__column row__column--compact">
-                                    <a href="/tags/{{ $tag->id }}/edit">
-                                        <i class="fa fa-pencil"></i>
-                                    </a>
-                                </div>
-                            </li>
+        <div class="spacing-bottom-large color-dark">Account</div>
+        <div class="box">
+            <div class="section">
+                <form method="POST">
+                    {{ csrf_field() }}
+                    <label>Name</label>
+                    <input type="text" name="name" value="{{ Auth::user()->name }}" />
+                    <label>E-mail</label>
+                    <input type="text" name="email" value="{{ Auth::user()->email }}" />
+                    <label>Language</label>
+                    <select name="language">
+                        @foreach ($languages as $language)
+                            <option value="{{ $language }}" @if (Auth::user()->language === $language) selected @endif>{{ $language }}</option>
                         @endforeach
-                    </ul>
-                </div>
+                    </select>
+                    <button>Update</button>
+                </form>
             </div>
+        </div>
+        <div class="spacing-top-large spacing-bottom-large color-dark">Tags</div>
+        <div class="box">
+            @if (count($tags))
+                <ul class="box__section">
+                    @foreach ($tags as $tag)
+                        <li class="row">
+                            <div class="row__column">{{ $tag->name }}</div>
+                            <div class="row__column row__column--compact">
+                                <a href="/tags/{{ $tag->id }}/edit">
+                                    <i class="fa fa-pencil"></i>
+                                </a>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <div class="box__section text-center">You don't have any tags</div>
+            @endif
         </div>
     </div>
 @endsection
