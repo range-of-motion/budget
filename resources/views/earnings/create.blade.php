d90271d170a4c9073c9049bf96e45cf087f0d2b0
Daniël van Drunen <daniel@pixely.me>
2018-08-14 11:41:29 +0200

    Semi clean up for views that are responsible for creating earnings/spendings

--- resources/views/earnings/create.blade.php
+++ resources/views/earnings/create.blade.php
@@ -4,22 +4,19 @@
     <div class="wrapper spacing-top-large spacing-bottom-large">
         <div class="box">
             <div class="section">
-                <h3>Earnings &middot; Create</h3>
-            </div>
-            <div class="section">
                 <form method="POST" action="/earnings">
                     {{ csrf_field() }}
-                    <div class="row gutter">
-                        <div class="column">
-                            <label>Date</label>
-                            <DatePicker></DatePicker>
-                        </div>
-                        <div class="column">
-                            <label>Description</label>
-                            <input type="text" name="description" />
-                            <label>Amount</label>
-                            <input type="text" name="amount" />
-                        </div>
+                    <div class="input input--small">
+                        <label>Date</label>
+                        <DatePicker></DatePicker>
+                    </div>
+                    <div class="input input--small">
+                        <label>Description</label>
+                        <input type="text" name="description" />
+                    </div>
+                    <div class="input input--small">
+                        <label>Amount</label>
+                        <input type="text" name="amount" />
                     </div>
                     <button>@lang('actions.create')</button>
                 </form>
--- resources/views/spendings/create.blade.php
+++ resources/views/spendings/create.blade.php
@@ -4,24 +4,29 @@
     <div class="wrapper spacing-top-large spacing-bottom-large">
         <div class="box">
             <div class="section">
-                <h3>Spendings &middot; Create</h3>
-            </div>
-            <div class="section">
                 <form method="POST" action="/spendings">
                     {{ csrf_field() }}
-                    <label>Tag</label>
-                    <select name="tag_id">
-                        <option value="">-</option>
-                        @foreach ($tags as $tag)
-                            <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                        @endforeach
-                    </select>
-                    <label>Date</label>
-                    <DatePicker></DatePicker>
-                    <label>Description</label>
-                    <input type="text" name="description" />
-                    <label>Amount</label>
-                    <input type="text" name="amount" />
+                    <div class="input input--small">
+                        <label>Tag</label>
+                        <select name="tag_id">
+                            <option value="">-</option>
+                            @foreach ($tags as $tag)
+                                <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="input input--small">
+                        <label>Date</label>
+                        <DatePicker></DatePicker>
+                    </div>
+                    <div class="input input--small">
+                        <label>Description</label>
+                        <input type="text" name="description" />
+                    </div>
+                    <div class="input input--small">
+                        <label>Amount</label>
+                        <input type="text" name="amount" />
+                    </div>
                     <button>@lang('actions.create')</button>
                 </form>
             </div>
