733cd4ff6e656f72834d07c3635ee98efb219b0a
Daniël van Drunen <daniel@pixely.me>
2018-08-16 01:29:23 +0200

    Add formatted_amount accessor to both Earning and Spending models

--- app/Earning.php
+++ app/Earning.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Earning extends Model {
     public $timestamps = false;
+
+    // Accessors
+    public function getFormattedAmountAttribute() {
+        return number_format($this->amount / 100, 2);
+    }
+
+    // Relations
+    //
 }
--- app/Spending.php
+++ app/Spending.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Spending extends Model {
     public $timestamps = false;
 
+    // Accessors
+    public function getFormattedAmountAttribute() {
+        return number_format($this->amount / 100, 2);
+    }
+
+    // Relations
     public function tag() {
         return $this->belongsTo('App\Tag');
     }
