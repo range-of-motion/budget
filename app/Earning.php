d8ffe6ee6f40c82513ed0a461e4e6d08e813a3dc
Daniël van Drunen <daniel@pixely.me>
2018-08-21 18:13:07 +0200

    Re-enable timestamps in Earning and Spending models

--- app/Earning.php
+++ app/Earning.php
@@ -5,8 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Earning extends Model {
-    public $timestamps = false;
-
     // Accessors
     public function getFormattedAmountAttribute() {
         return number_format($this->amount / 100, 2);
