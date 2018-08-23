482500c12d035d658bd5571a9c0ea894c78f9e9f
Daniël van Drunen <daniel@pixely.me>
2018-08-23 15:17:29 +0200

    Get rid of Chart.js inclusion in layout view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,6 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:400,400i" />
         <link rel="stylesheet" href="/css/app.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
         <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css" />
         <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
         <style>
