2fdce91766534054c4bddd39dd25d7e0d06ad712
Daniël van Drunen <daniel@pixely.me>
2018-08-28 21:12:46 +0200

    Add translations for required, confirmed and max.string validation rules

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'required' => 'Can\'t be blank',
+    'confirmed' => 'Doesn\'t match',
+    'max' => [
+        'string' => 'Can\'t be longer than :max characters'
+    ]
+];
---
+++ resources/lang/nl/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'required' => 'Kan niet leeg zijn',
+    'confirmed' => 'Komt niet overeen',
+    'max' => [
+        'string' => 'Kan niet langer zijn dan :max karakters'
+    ]
+];
