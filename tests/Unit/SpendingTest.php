4e9b73b874c6cdddb49c29c789167a98ed566d8f
Daniël van Drunen <daniel@pixely.me>
2018-09-02 13:49:05 +0200

    Add test for formatted_amount attribute of spendings

---
+++ tests/Unit/SpendingTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Spending;
+
+class SpendingTest extends TestCase {
+    public function testFormattedAmount() {
+        $spending = new Spending;
+
+        $spending->user_id = 1;
+        // $spending->tag_id = null;
+        $spending->happened_on = date('Y-m-d');
+        $spending->description = 'SpendingTest testExample';
+        $spending->amount = 92;
+        $spending->save();
+
+        $this->assertEquals('92.00', $spending->formatted_amount);
+    }
+}
