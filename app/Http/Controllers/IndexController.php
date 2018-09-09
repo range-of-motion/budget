c809078389dd01f4cffffdebc77e8d6f720bef65
Daniël van Drunen <daniel@pixely.me>
2018-09-09 21:06:50 +0200

    Get rid of index view and redirect user to login or dashboard route

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class IndexController extends Controller {
     public function index() {
-        return view('index');
+        if (Auth::check()) {
+            return redirect()->route('dashboard');
+        }
+
+        return redirect()->route('login');
     }
 }
