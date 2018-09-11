e2e1dfc9f629a1019493979d62f083c26ea21cb3
Daniël van Drunen <daniel@pixely.me>
2018-09-11 02:28:45 +0200

    Update factories to equivalent of those that come with Laravel 5.6 by default

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,14 +11,11 @@
 |
 */
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
-
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
 });
