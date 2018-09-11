1619ab9e3122476e3063e61e32fd272517589c53
Daniël van Drunen <daniel@pixely.me>
2018-09-12 00:51:59 +0200

    Add factory for Spending model

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,12 +1,22 @@
 <?php
 
+use Faker\Generator;
 use App\User;
+use App\Spending;
 
-$factory->define(User::class, function (Faker\Generator $faker) {
+$factory->define(User::class, function (Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10)
+    ];
+});
+
+$factory->define(Spending::class, function (Generator $faker) {
+    return [
+        'happened_on' => $faker->dateTimeBetween('-50 days', 'now')->format('Y-m-d'),
+        'description' => implode(' ', array_map('ucfirst', $faker->words(3))),
+        'amount' => $faker->randomNumber(3)
     ];
 });
