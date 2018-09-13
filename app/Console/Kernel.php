eeb660c381d52efd1325106af80fe4b8b4679ebc
Daniël van Drunen <daniel@pixely.me>
2018-09-13 17:33:32 +0200

    Add ProcessRecurrings job and schedule it for execution every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\ProcessRecurrings;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->job(new ProcessRecurrings)->everyMinute();
     }
 
     /**
---
+++ app/Jobs/ProcessRecurrings.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Recurring;
+use App\Spending;
+
+class ProcessRecurrings implements ShouldQueue {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct() {
+        //
+    }
+
+    public function handle() {
+        $recurrings = Recurring
+            ::where('type', 'monthly')
+            ->where('day', date('j'))
+            ->where('starts_on', '<=', date('Y-m-d'))
+            ->where('ends_on', '>=', date('Y-m-d'))
+            ->where(function ($query) {
+                $query
+                    ->where('last_used_on', date('j'))
+                    ->orWhere('last_used_on', null);
+            })->get();
+
+        foreach ($recurrings as $recurring) {
+            $spending = new Spending;
+            $spending->user_id = $recurring->user_id;
+            $spending->recurring_id = $recurring->id;
+            $spending->tag_id = $recurring->tag_id;
+            $spending->happened_on = date('Y-m-d');
+            $spending->description = $recurring->description;
+            $spending->amount = $recurring->amount;
+            $spending->save();
+
+            $recurring->last_used_on = date('Y-m-d');
+            $recurring->save();
+        }
+    }
+}
