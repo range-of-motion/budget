5020eaf56e70403fbd01acf7f1f5ddf4cff1376a
Daniël van Drunen <daniel@pixely.me>
2018-09-17 00:21:55 +0200

    Add factory for Earning model

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator;
 use App\User;
+use App\Earning;
 use App\Spending;
 
 $factory->define(User::class, function (Generator $faker) {
@@ -10,6 +11,14 @@ $factory->define(User::class, function (Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10)
+    ];
+});
+
+$factory->define(Earning::class, function (Generator $faker) {
+    return [
+        'happened_on' => $faker->dateTimeBetween('-50 days', 'now')->format('Y-m-d'),
+        'description' => implode(' ', array_map('ucfirst', $faker->words(3))),
+        'amount' => $faker->randomNumber(3)
     ];
 });
 
