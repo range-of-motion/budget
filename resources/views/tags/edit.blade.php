f351a46663aa46c784070731adee715a966bedb5
Daniël van Drunen <daniel@pixely.me>
2018-09-17 19:32:25 +0200

    Minor change to interface seen while editing tags

--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -3,18 +3,24 @@
 @section('body')
     <div class="wrapper my-4">
         <div class="box">
-            <div class="box__section">
-                <h3>Tags &middot; Edit</h3>
-            </div>
-            <div class="box__section">
-                <form method="POST" action="/tags/{{ $tag->id }}">
-                    {{ method_field('PATCH') }}
-                    {{ csrf_field() }}
-                    <label>Name</label>
-                    <input type="text" name="name" value="{{ $tag->name }}" />
-                    <button class="button">Update</button>
-                </form>
-            </div>
+            <form method="POST" action="/tags/{{ $tag->id }}">
+                {{ method_field('PATCH') }}
+                {{ csrf_field() }}
+                <div class="box__section">
+                    <div class="input input--small">
+                        <label>Name</label>
+                        <input type="text" name="name" value="{{ $tag->name }}" />
+                    </div>
+                </div>
+                <div class="box__section row row--right">
+                    <div class="row__column row__column--compact row__column--middle">
+                        <a href="/tags">Cancel</a>
+                    </div>
+                    <div class="row__column row__column--compact ml-2">
+                        <button class="button">Update</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
