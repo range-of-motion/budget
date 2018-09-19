f0f40644f8e1036e863671ca750d640f5ec50166
Daniël van Drunen <daniel@pixely.me>
2018-09-19 02:27:09 +0200

    Refresh UI for tags' create view

--- resources/views/tags/create.blade.php
+++ resources/views/tags/create.blade.php
@@ -1,19 +1,27 @@
 @extends('layout')
 
+@section('title', __('actions.create') . ' ' . __('general.tag'))
+
 @section('body')
     <div class="wrapper my-4">
         <div class="box">
-            <div class="box__section">
-                <h3>Tags &middot; Create</h3>
-            </div>
-            <div class="box__section">
-                <form method="POST" action="/tags">
-                    {{ csrf_field() }}
-                    <label>Name</label>
-                    <input type="text" name="name" />
-                    <button class="button">@lang('actions.create')</button>
-                </form>
-            </div>
+            <form method="POST" action="/tags">
+                {{ csrf_field() }}
+                <div class="box__section">
+                    <div class="input input--small">
+                        <label>Name</label>
+                        <input type="text" name="name" />
+                    </div>
+                </div>
+                <div class="box__section row row--right">
+                    <div class="row__column row__column--compact row__column--middle">
+                        <a href="/tags">Cancel</a>
+                    </div>
+                    <div class="row__column row__column--compact ml-2">
+                        <button class="button">{{ __('actions.create') }}</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
