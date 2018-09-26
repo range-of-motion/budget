ef0e706bf27b930a89d688c93269ebc3f42b0ad1
Daniël van Drunen <daniel@pixely.me>
2018-09-26 16:15:37 +0200

    Update styling in views used for 403 and 404 errors

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,7 @@
 @extends('layout')
 
 @section('body')
-    <div class="wrapper narrow spacing-top-large spacing-bottom-large">
-        <div class="box">
-            <div class="section">
-                <h3 class="text-align-center">404</h3>
-            </div>
-        </div>
+    <div class="wrapper wrapper--narrow my-3">
+        <h2 class="text-center">404</h2>
     </div>
 @endsection
