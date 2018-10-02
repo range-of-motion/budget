f7f7bcdf2381ded68147141b73f5d57e2a0ed3c1
Daniël van Drunen <daniel@pixely.me>
2018-10-02 21:08:28 +0200

    Create migration for user_space table

---
+++ database/migrations/2018_10_02_190502_create_user_space_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserSpaceTable extends Migration {
+    public function up() {
+        Schema::create('user_space', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('space_id');
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('user_space');
+    }
+}
