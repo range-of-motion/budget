f55e8248eabfda9e2a4ee87c33555d3d8d70477e
Daniël van Drunen <daniel@pixely.me>
2018-10-05 20:41:09 +0200

    Create RefreshSpaceRelations middleware for refreshing relations of selected space that's stored in session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\SetLocale::class
+            \App\Http\Middleware\SetLocale::class,
+            \App\Http\Middleware\RefreshSpaceRelations::class
         ],
 
         'api' => [
---
+++ app/Http/Middleware/RefreshSpaceRelations.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RefreshSpaceRelations {
+    public function handle($request, Closure $next) {
+        if ($request->session()->has('space')) {
+            $request->session()->get('space')->load('tags', 'earnings', 'spendings', 'recurrings');
+        }
+
+        return $next($request);
+    }
+}
