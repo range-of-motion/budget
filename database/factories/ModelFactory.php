e6c797b144ff4ab7c538ec01208105786c98cabc
Daniël van Drunen <daniel@pixely.me>
2018-10-06 09:43:21 +0200

    Add factory for Space model and create space for every new user in UsersTableSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator;
 use App\User;
+use App\Space;
 use App\Earning;
 use App\Spending;
 
@@ -12,6 +13,12 @@ $factory->define(User::class, function (Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10)
+    ];
+});
+
+$factory->define(Space::class, function (Generator $faker) {
+    return [
+        'name' => $faker->firstName() . '\'s Space'
     ];
 });
 
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,9 +3,14 @@
 use Illuminate\Database\Seeder;
 
 use App\User;
+use App\Space;
 
 class UsersTableSeeder extends Seeder {
     public function run() {
-        factory(User::class, 5)->create();
+        factory(User::class, 5)->create()->each(function ($user) {
+            $user->spaces()->attach(factory(Space::class)->create([
+                'name' => $user->name . '\'s Space'
+            ]));
+        });
     }
 }
