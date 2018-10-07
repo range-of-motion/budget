b3970219c2061634da8880d0eb51a423fcf71ef6
Daniël van Drunen <daniel@pixely.me>
2018-10-07 18:49:55 +0200

    Add ability to delete earnings from earnings' index view

--- resources/views/earnings/index.blade.php
+++ resources/views/earnings/index.blade.php
@@ -13,7 +13,16 @@
                             <div class="color-dark">{{ $earning->description }}</div>
                             <div class="mt-1" style="font-size: 14px; font-weight: 600;">{{ $earning->formatted_happened_on }}</div>
                         </div>
-                        <div class="row__column row__column--middle color-dark text-right">&euro; {{ $earning->formatted_amount }}</div>
+                        <div class="row__column row__column--middle color-dark">&euro; {{ $earning->formatted_amount }}</div>
+                        <div class="row__column row__column--middle row__column--compact">
+                            <form method="POST" action="/earnings/{{ $earning->id }}">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button class="button link">
+                                    <i class="far fa-trash-alt"></i>
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 @endforeach
             @else
