2c53a0f5517e0c0c88d39cacd05858f2fd21e59d
Daniël van Drunen <daniel@pixely.me>
2018-10-09 19:17:01 +0200

    Create SendWeeklyReports job

---
+++ app/Jobs/SendWeeklyReports.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\WeeklyReport;
+use App\Space;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendWeeklyReports implements ShouldQueue {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct() {
+        //
+    }
+
+    public function handle() {
+        $spaces = Space::all();
+
+        foreach ($spaces as $space) {
+            // TODO CALCULATE STATISTICS
+
+            foreach ($space->users as $user) {
+                // Only send if user wants to receive report
+                if ($user->weekly_report) {
+                    Mail::to($user->email)->queue(new WeeklyReport($space));
+                }
+            }
+        }
+    }
+}
