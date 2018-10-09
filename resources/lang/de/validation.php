e19836e052c6f6b53c449d7439a89c3dd46d503f
wellá <weller.andrethomas@web.de>
2018-10-09 21:38:58 +0200

    Add german translation to validation.php

---
+++ resources/lang/de/validation.php
@@ -0,0 +1,9 @@
+
+<?php
+return [
+    'required' => 'Darf nicht leer sein',
+    'confirmed' => 'Stimmt nicht überein',
+    'max' => [
+        'string' => 'Darf nicht länger als :max Zeichen sein'
+    ]
+];
