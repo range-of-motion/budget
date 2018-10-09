f653e73367aaf4009debd0315d8017f29396d3ad
wellá <weller.andrethomas@web.de>
2018-10-09 21:37:58 +0200

    added german translation to general

---
+++ resources/lang/de/general.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'dashboard' => 'Dashboard',
+    'total_spent' => 'Ausgaben Gesamt',
+    'most_expensive_tag' => 'Höchste Ausgaben Label',
+    'most_expensive_day' => 'Höchste Ausgaben Tag',
+    'analysis' => 'Analyse',
+    'earnings' => 'Einnahmen',
+    'earning' => 'Einnahme',
+    'spendings' => 'Ausgaben',
+    'spending' => 'Ausgabe',
+    'tags' => 'Label',
+    'tag' => 'Label',
+    'recent' => 'Zuletzt',
+    'avatar' => 'Avatar',
+    'name' => 'Name',
+    'email' => 'E-mail',
+    'verify' => 'Bestätigen',
+    'password' => 'Passwort',
+    'language' => 'Sprache',
+    'theme' => 'Design',
+    'recurrings' => 'Zyklisch'
+];
