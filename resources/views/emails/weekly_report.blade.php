b75465d68d7167fcb19eca5baea99e59e26cbb8d
Daniël van Drunen <daniel@pixely.me>
2018-10-10 18:28:57 +0200

    Wrap statistics in weekly report in a list

--- resources/views/emails/weekly_report.blade.php
+++ resources/views/emails/weekly_report.blade.php
@@ -1,7 +1,9 @@
 Here's your weekly report for {{ $space->name }}.
 
 This week (#{{ $week }}) you've
-* Spent CURRENCY SPENT_TOTAL_AMOUNT
-* Most of which you've spent on SPENT_MOST_TAG_NAME (CURRENCY SPENT_MOST_TAG_AMOUNT)
+<ul>
+    <li>Spent CURRENCY SPENT_TOTAL_AMOUNT</li>
+    <li>Most of which you've spent on SPENT_MOST_TAG_NAME (CURRENCY SPENT_MOST_TAG_AMOUNT)</li>
+</ul>
 
 If you don't want to receive a weekly report, head over to <a href="{{ config('app.url') . '/settings' }}">settings</a> and let us know.
