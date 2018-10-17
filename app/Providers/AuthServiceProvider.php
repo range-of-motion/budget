d2f0e5f060a4b58efcb860763cf87080ae87b67a
Daniël van Drunen <daniel@pixely.me>
2018-10-17 23:46:14 +0200

    Stop using FQCNs in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Earning;
 use App\Import;
+use App\Policies\EarningPolicy;
 use App\Policies\ImportPolicy;
+use App\Policies\SpendingPolicy;
+use App\Policies\TagPolicy;
+use App\Spending;
+use App\Tag;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Policies\RecurringPolicy;
@@ -18,10 +24,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
-        'App\Earning' => 'App\Policies\EarningPolicy',
-        'App\Spending' => 'App\Policies\SpendingPolicy',
+        Earning::class => EarningPolicy::class,
+        Spending::class => SpendingPolicy::class,
         Recurring::class => RecurringPolicy::class,
-        'App\Tag' => 'App\Policies\TagPolicy',
+        Tag::class => TagPolicy::class,
         Import::class => ImportPolicy::class
     ];
 
