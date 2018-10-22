62b41ca2dd7c7a867c3bea9200276f5e9dc0f682
Daniël van Drunen <daniel@pixely.me>
2018-10-22 08:17:05 +0200

    Use layout view in both prepare and complete views for imports

--- resources/views/imports/complete.blade.php
+++ resources/views/imports/complete.blade.php
@@ -1,29 +1,46 @@
-<form method="POST">
-    {{ csrf_field() }}
-    <div style="display: flex;">
-        <div>Import</div>
-        <div>{{ __('fields.date') }}</div>
-        <div>{{ __('fields.description') }}</div>
-        <div>{{ __('fields.amount') }}</div>
-    </div>
-    @foreach ($rows as $index => $row)
-        <div style="display: flex;">
-            <div>
-                <input type="checkbox" name="rows[{{ $index }}][import]" />
-            </div>
-            <div>
-                <input type="text" name="rows[{{ $index }}][happened_on]" value="{{ $row['happened_on'] }}" />
-                {{ $errors->first('rows.' . $index . '.happened_on') }}
-            </div>
-            <div>
-                <input type="text" name="rows[{{ $index }}][description]" value="{{ $row['description'] }}" />
-                {{ $errors->first('rows.' . $index . '.description') }}
-            </div>
-            <div>
-                <input type="text" name="rows[{{ $index }}][amount]" value="{{ $row['amount'] }}" />
-                {{ $errors->first('rows.' . $index . '.amount') }}
-            </div>
+@extends('layout')
+
+@section('title', 'Complete ' . __('models.import'))
+
+@section('body')
+    <div class="wrapper my-3">
+        <h2 class="mb-3">Complete {{ __('models.import') }}</h2>
+        <div class="box">
+            <form method="POST">
+                {{ csrf_field() }}
+                <div class="box__section box__section--header">
+                    <div class="row row--gutter">
+                        <div class="row__column row__column--compact" style="width: 100px;">Import</div>
+                        <div class="row__column">{{ __('fields.date') }}</div>
+                        <div class="row__column">{{ __('fields.description') }}</div>
+                        <div class="row__column">{{ __('fields.amount') }}</div>
+                    </div>
+                </div>
+                @foreach ($rows as $index => $row)
+                    <div class="box__section">
+                        <div class="row row--gutter">
+                            <div class="row__column row__column--compact" style="width: 100px;">
+                                <input type="checkbox" name="rows[{{ $index }}][import]" />
+                            </div>
+                            <div class="row__column">
+                                <input type="text" name="rows[{{ $index }}][happened_on]" value="{{ $row['happened_on'] }}" />
+                                {{ $errors->first('rows.' . $index . '.happened_on') }}
+                            </div>
+                            <div class="row__column">
+                                <input type="text" name="rows[{{ $index }}][description]" value="{{ $row['description'] }}" />
+                                {{ $errors->first('rows.' . $index . '.description') }}
+                            </div>
+                            <div class="row__column">
+                                <input type="text" name="rows[{{ $index }}][amount]" value="{{ $row['amount'] }}" />
+                                {{ $errors->first('rows.' . $index . '.amount') }}
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="box__section box__section--highlight text-right">
+                    <button class="button">Submit</button>
+                </div>
+            </form>
         </div>
-    @endforeach
-    <button>Submit</button>
-</form>
+    </div>
+@endsection
