c0a8c6451967ade68d46e2ed094540ffacd1d2e4
Daniël van Drunen <daniel@pixely.me>
2018-10-28 23:42:38 +0100

    Test authorized user's access to editing of tags

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -10,6 +10,24 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TagTest extends TestCase {
+    public function testAuthorizedUserCanEditTag() {
+        $user = factory(User::class)->create();
+
+        $space = factory(Space::class)->create();
+
+        $user->spaces()->sync([$space->id]);
+
+        $tag = factory(Tag::class)->create([
+            'space_id' => $space->id
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get('/tags/' . $tag->id . '/edit');
+
+        $response->assertStatus(200);
+    }
+
     public function testUnauthorizedUserCantEditTag() {
         $user = factory(User::class)->create();
 
