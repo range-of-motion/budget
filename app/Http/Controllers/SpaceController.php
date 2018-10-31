60891c4a80bc640dc731eb687c3380037436a792
Daniël van Drunen <daniel@pixely.me>
2018-10-31 23:37:28 +0100

    Fix typo and bug with permissions in SpaceController

--- app/Http/Controllers/SpaceController.php
+++ app/Http/Controllers/SpaceController.php
@@ -4,16 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Space;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SpaceController extends Controller {
     public function __invoke($id) {
         $space = Space::find($id);
 
-        if ($space->users->contains(Auh::user()->id)) {
-            return redirect()->route('dashboard');
+        if ($space->users->contains(Auth::user()->id)) {
+            session(['space' => $space]);
         }
-
-        session(['space' => $space]);
 
         return redirect()->route('dashboard');
     }
