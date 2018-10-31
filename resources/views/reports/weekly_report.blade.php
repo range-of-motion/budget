03ea2f11af65493f74fbe42099b70000ab4bfe46
Daniël van Drunen <daniel@pixely.me>
2018-10-31 08:42:18 +0100

    Render months instead of weeks underneath chart in weekly report

--- resources/views/reports/weekly_report.blade.php
+++ resources/views/reports/weekly_report.blade.php
@@ -13,14 +13,46 @@
 
 @section('scripts')
     <script>
+        function getWeeksInMonth(year, month) {
+            var firstOfMonth = new Date(year, month - 1, 1);
+            var lastOfMonth = new Date(year, month, 0);
+
+            var used = firstOfMonth.getDay() + lastOfMonth.getDate();
+
+            return Math.ceil(used / 7);
+        }
+
         var data = {
             labels: [{!! implode(',', array_keys($weeks)) !!}],
             series: [[{!! '"' . implode('","', $weeks) . '"' !!}]]
         };
 
+        var currentMonth = 1;
+        var offset = 0;
+        var months = [];
+
+        for (var i = 1; i <= 12; i ++) {
+            months[i] = getWeeksInMonth(2018, i);
+        }
+
         new Chartist.Line('.ct-chart', data, {
             showPoint: false,
-            lineSmooth: false
+            lineSmooth: false,
+
+            axisX: {
+                labelInterpolationFnc: function (value, index) {
+                    offset ++;
+
+                    if (months[currentMonth] == offset) {
+                        currentMonth ++;
+                        offset = 1;
+
+                        return currentMonth - 1;
+                    }
+
+                    return null;
+                }
+            }
         });
     </script>
 @endsection
