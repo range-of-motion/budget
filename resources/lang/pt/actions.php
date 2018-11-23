e024a221b5acde225ff028d145174c75b44e773c
Jorge Dacosta <jorgemsdc@mail.com>
2018-11-23 21:49:01 +0000

    🌍 adding portuguese resources

---
+++ resources/lang/pt/actions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'cancel' => 'Cancelar',
+    'create' => 'Criar',
+    'edit' => 'Editar',
+    'save' => 'Salvar',
+    'delete' => 'Deletar',
+
+    'verify' => 'Verificar',
+
+    'yes' => 'Sim',
+    'no' => 'Não'
+];
---
+++ resources/lang/pt/models.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'spaces' => 'Espaços',
+    'space' => 'Espaço',
+
+    'tags' => 'Etiquetas',
+    'tag' => 'Etiqueta',
+
+    'recurrings' => 'Recorrências',
+    'recurring' => 'Recorrência',
+
+    'earnings' => 'Ganhos',
+    'earning' => 'Ganho',
+
+    'spendings' => 'Gastos',
+    'spending' => 'Gasto',
+
+    'imports' => 'Importações',
+    'import' => 'Importar'
+];
