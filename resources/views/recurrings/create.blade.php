1cc59a9c86d39f8b1a4d3530ae10c93230fc97e2
Daniël van Drunen <daniel@pixely.me>
2018-12-11 23:16:41 +0100

    Move elements in view for creating recurring below each other

--- resources/views/recurrings/create.blade.php
+++ resources/views/recurrings/create.blade.php
@@ -3,59 +3,51 @@
 @section('title', __('actions.create') . ' ' . __('models.recurring'))
 
 @section('body')
-    <div class="wrapper my-3">
-        <h2>{{ __('actions.create') }} {{ __('models.recurring') }}</h2>
-        <div class="box mt-3">
-            <form method="POST" action="/recurrings" autocomplete="off">
-                {{ csrf_field() }}
-                <div class="box__section row">
-                    <div class="row__column">
-                        <div class="input input--small">
-                            <label>Day</label>
-                            <input type="text" name="day" />
-                            <div style="font-weight: 700; font-size: 14px; margin-top: 5px;">1 - 28</div>
-                            @include('partials.validation_error', ['payload' => 'day'])
-                        </div>
-                        <div class="input input--small mb-0">
-                            <label>End</label>
-                            <DatePicker name="end"></DatePicker>
-                            @include('partials.validation_error', ['payload' => 'end'])
-                            <input type="checkbox" name="end" value="" id="endForever" />
-                            <label for="endForever">Forever</label>
-                        </div>
-                    </div>
-                    <div class="row__column">
-                        <div class="input input--small">
-                            <label>Tag</label>
-                            <select name="tag">
-                                <option value="">-</option>
-                                @foreach ($tags as $tag)
-                                    <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                                @endforeach
-                            </select>
-                            @include('partials.validation_error', ['payload' => 'tag'])
-                        </div>
-                        <div class="input input--small">
-                            <label>Description</label>
-                            <input type="text" name="description" />
-                            @include('partials.validation_error', ['payload' => 'description'])
-                        </div>
-                        <div class="input input--small mb-0">
-                            <label>Amount</label>
-                            <input type="text" name="amount" />
-                            @include('partials.validation_error', ['payload' => 'amount'])
-                        </div>
-                    </div>
+    <div class="wrapper mw-400 my-3">
+        <h2 class="mb-3">{{ __('actions.create') }} {{ __('models.recurring') }}</h2>
+        <form method="POST" action="/recurrings" autocomplete="off">
+            {{ csrf_field() }}
+            <div class="input input--small">
+                <label>Day</label>
+                <input type="text" name="day" />
+                <div style="font-weight: 700; font-size: 14px; margin-top: 5px;">1 - 28</div>
+                @include('partials.validation_error', ['payload' => 'day'])
+            </div>
+            <div class="input input--small">
+                <label>End</label>
+                <DatePicker name="end"></DatePicker>
+                @include('partials.validation_error', ['payload' => 'end'])
+                <input type="checkbox" name="end" value="" id="endForever" />
+                <label for="endForever">Forever</label>
+            </div>
+            <div class="input input--small">
+                <label>Tag</label>
+                <select name="tag">
+                    <option value="">-</option>
+                    @foreach ($tags as $tag)
+                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+                @include('partials.validation_error', ['payload' => 'tag'])
+            </div>
+            <div class="input input--small">
+                <label>Description</label>
+                <input type="text" name="description" />
+                @include('partials.validation_error', ['payload' => 'description'])
+            </div>
+            <div class="input input--small">
+                <label>Amount</label>
+                <input type="text" name="amount" />
+                @include('partials.validation_error', ['payload' => 'amount'])
+            </div>
+            <div class="row row--right">
+                <div class="row__column row__column--compact row__column--middle">
+                    <a href="/tags">Cancel</a>
                 </div>
-                <div class="box__section box__section--highlight row row--right">
-                    <div class="row__column row__column--compact row__column--middle">
-                        <a href="/tags">Cancel</a>
-                    </div>
-                    <div class="row__column row__column--compact ml-2">
-                        <button class="button">{{ __('actions.create') }}</button>
-                    </div>
+                <div class="row__column row__column--compact ml-2">
+                    <button class="button">{{ __('actions.create') }}</button>
                 </div>
-            </form>
-        </div>
+            </div>
+        </form>
     </div>
 @endsection
