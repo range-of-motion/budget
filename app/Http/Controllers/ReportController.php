fe97bf991f4c67eaa3a03240fcefc28f3b551080
Daniël van Drunen <daniel@pixely.me>
2019-01-05 20:59:41 +0100

    Move logic for reports to separate functions in their controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,33 +10,32 @@ class ReportController extends Controller {
         return view('reports.index');
     }
 
-    public function show($slug) {
-        if ($slug == 'weekly-report-2018') {
-            $weeks = [];
-            $balance = 0;
+    private function weeklyReport2018() {
+        $weeks = [];
+        $balance = 0;
 
-            for ($i = 1; $i <= 52; $i ++) {
-                $balance += session('space')
-                    ->earnings()
-                    ->whereRaw('YEARWEEK(happened_on) = ?', [2018 . sprintf('%02d', $i)])
-                    ->sum('amount');
+        for ($i = 1; $i <= 52; $i ++) {
+            $balance += session('space')
+                ->earnings()
+                ->whereRaw('YEARWEEK(happened_on) = ?', [2018 . sprintf('%02d', $i)])
+                ->sum('amount');
 
-                $balance -= session('space')
-                    ->spendings()
-                    ->whereRaw('YEARWEEK(happened_on) = ?', [2018 . sprintf('%02d', $i)])
-                    ->sum('amount');
+            $balance -= session('space')
+                ->spendings()
+                ->whereRaw('YEARWEEK(happened_on) = ?', [2018 . sprintf('%02d', $i)])
+                ->sum('amount');
 
-                $weeks[$i] = number_format($balance / 100, 2, '.', '');
-            }
-
-            return view('reports.weekly_report', [
-                'year' => 2018,
-                'weeks' => $weeks
-            ]);
+            $weeks[$i] = number_format($balance / 100, 2, '.', '');
         }
 
-        if ($slug == 'most-expensive-tags') {
-            $mostExpensiveTags = DB::select('
+        return view('reports.weekly_report', [
+            'year' => 2018,
+            'weeks' => $weeks
+        ]);
+    }
+
+    private function mostExpensiveTags() {
+        $mostExpensiveTags = DB::select('
                 SELECT
                     tags.name AS name,
                     tags.color AS color,
@@ -55,11 +54,21 @@ class ReportController extends Controller {
                     SUM(spendings.amount) DESC;
             ', [session('space')->id]);
 
-            $totalSpent = session('space')->spendings()->sum('amount');
+        $totalSpent = session('space')->spendings()->sum('amount');
 
-            return view('reports.most_expensive_tags', compact('mostExpensiveTags', 'totalSpent'));
+        return view('reports.most_expensive_tags', compact('mostExpensiveTags', 'totalSpent'));
+    }
+
+    public function show($slug) {
+        switch ($slug) {
+            case 'weekly-report-2018':
+                return $this->weeklyReport2018();
+
+            case 'most-expensive-tags':
+                return $this->mostExpensiveTags();
+
+            default:
+                return '404';
         }
-
-        return '404';
     }
 }
