5cc1ce7d8d078dafe32238accb0afa59ed631ba4
Daniël van Drunen <daniel@pixely.me>
2019-01-12 21:22:58 +0100

    Display list of reports as cards instead of unordered list

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -5,12 +5,21 @@
 @section('body')
     <div class="wrapper my-3">
         <h2>Reports</h2>
-        <div class="box mt-3">
-            <div class="box__section">
-                <a href="/reports/weekly-report">Weekly Balance</a>
+        <div class="row row--gutter row--responsive mt-3">
+            <div class="row__column">
+                <div class="card">
+                    <a href="/reports/weekly-report">Weekly Balance</a>
+                    <p class="mt-1">Your balance over the course of 52 weeks&mdash;displayed in a graph.</p>
+                </div>
             </div>
-            <div class="box__section">
-                <a href="/reports/most-expensive-tags">Most Expensive Tags</a>
+            <div class="row__column">
+                <div class="card">
+                    <a href="/reports/most-expensive-tags">Most Expensive Tags</a>
+                    <p class="mt-1">Your all-time most expensive tags&mdash;find out what costs you the most.</p>
+                </div>
+            </div>
+            <div class="row__column">
+                <!-- EMPTY -->
             </div>
         </div>
     </div>
