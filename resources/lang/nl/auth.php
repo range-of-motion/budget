4dd1ced16ba3b23638b0da301e1fd9609ddbae59
Sebastiaan Speck <shem.speck@gmail.com>
2019-07-27 08:02:39 +0200

    Added Dutch translations

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'login' => 'Login',
+    'reset_password' => 'Wachtwoord Resetten',
+
+    'register' => 'Registereren',
+    'verify_password' => 'Herhaal Wachtwoord',
+
+    'forgot_password' => 'Je wachtwoord vergeten?',
+    'new_to_budget' => 'Nieuw bij Budget?',
+    'already_on_budget' => 'Al aangemeld bij Budget?'
+];
--- resources/lang/nl/general.php
+++ resources/lang/nl/general.php
@@ -3,9 +3,12 @@
 return [
     'dashboard' => 'Overzicht',
 
+    'balance' => 'Saldo',
+    'daily_balance' => 'Dagelijks Saldo',
+    'left_to_spend' => 'Resterend Te Besteden',
     'total_earned' => 'Totaal Verdiend',
     'total_spent' => 'Totaal Uitgegeven',
-    'most_expensive_tag' => 'Duurste Etiket',
+    'most_expensive_tag' => 'Duurste Categroie',
     'most_expensive_day' => 'Duurste Dag',
 
     'analysis' => 'Analyse',
@@ -18,7 +21,9 @@ return [
 
     'empty_state' => 'Er zijn nog geen :resource',
 
-    'verify_account' => 'Je moet je account nog verifiëren&mdash;check je e-mail',
-    'spaces_explanation' => 'Spaces kunnen worden gebruikt om je financiën op sorteren in Budget. Bijvoorbeeld—je kan een space hebben voor jou persoonlijk en een andere voor je bedrijf.',
-    'know_how_to_make_this_app_better' => 'Weet je wat er beter kan'
+    'verify_account' => 'Je moet je account nog verifiëren — check je e-mail',
+    'spaces_explanation' => 'Spaces kunnen worden gebruikt om je financiën op sorteren in Budget. Bijvoorbeeld — je kan een space hebben voor jou persoonlijk en een andere voor je bedrijf.',
+    'know_how_to_make_this_app_better' => 'Weet je wat er beter kan?',
+    'successfully_deleted' => 'Je hebt succesvol die :resource verwijderd',
+    'still_able_to_recover' => 'Je kan hem nog herstellen',
 ];
