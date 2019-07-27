551763af0354ba4a1b1370b4122f6a055c54f754
Sebastiaan Speck <shem.speck@gmail.com>
2019-07-27 13:55:16 +0200

    bugfix

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -38,6 +38,7 @@
                                     <h1 style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; color: #2F3133; font-size: 19px; font-weight: bold; margin-top: 0; text-align: left;">
                                         @yield('title')
                                     </h1>
+                                    @yield('message')
                                     <table class="action" align="center" width="100%" cellpadding="0" cellspacing="0" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; margin: 30px auto; padding: 0; text-align: center; width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 100%;">
                                         <tr>
                                             <td align="center" style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box;">
