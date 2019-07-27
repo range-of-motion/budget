c5a76b9c48850f188c34fad311b42b311f1cf71f
Sebastiaan Speck <shem.speck@gmail.com>
2019-07-27 10:23:59 +0200

    Translated the Vue-components

--- resources/assets/js/vue-i18n-locales.generated.js
+++ resources/assets/js/vue-i18n-locales.generated.js
@@ -188,10 +188,13 @@ export default {
             "submit": "Submit",
             "reset": "Reset",
             "verify": "Verify",
+            "select": "Select",
+            "search": "Search",
+            "filter_by": "Filter by",
             "yes": "Yes",
             "no": "No",
-            "previous": "Previous",
-            "next": "Next",
+            "previous": "« Previous",
+            "next": "Next »",
             "prepare": "Prepare",
             "complete": "Complete"
         },
@@ -402,15 +405,31 @@ export default {
             "profile": "Profile",
             "account": "Account",
             "preferences": "Preferences",
+            "themes": {
+                "light": "Light",
+                "dark": "Dark (Experimental)"
+            }
+        },
+        "messages": {
             "empty_state": "There aren't any {resource} (yet)",
             "verify_account": "You still need to verify your account — please check your e-mail",
+            "email_sent": "If you registered with that address, we've sent you an e-mail",
+            "password_changed": "Your password has been changed",
+            "login_failed": "Failed to login",
+            "verified_account": "You've successfully verified your account",
             "spaces_explanation": "Spaces can be used to separate your finances in Budget. For example — you can have a space for your personal finances and another space for your business' finances.",
             "know_how_to_make_this_app_better": "Know how to make this app better?",
             "successfully_deleted": "You've successfully deleted that {resource}",
             "still_able_to_recover": "You can still recover it",
-            "themes": {
-                "light": "Light",
-                "dark": "Dark (Experimental)"
+            "transaction_wizard": {
+                "new_earning_description": "Paycheck February",
+                "new_spending_description": "Birthday Present for Angela",
+                "recurring_explanation": "This is a recurring transaction",
+                "recurring_duration": "How long will this transaction go on for?",
+                "forever": "Forever :(",
+                "until": "Until",
+                "loading": "Loading",
+                "successfully_created": "You've successfully created that transaction"
             }
         },
         "pages": {
@@ -539,10 +558,13 @@ export default {
             "submit": "Verstuur",
             "reset": "Reset",
             "verify": "Controleer",
+            "select": "Selecteer",
+            "search": "Zoeken",
+            "filter_by": "Filteren op",
             "yes": "Ja",
             "no": "Nee",
-            "previous": "Vorige",
-            "next": "Volgende",
+            "previous": "« Vorige",
+            "next": "Volgende »",
             "prepare": "Voorbereiden",
             "complete": "Voltooien"
         },
@@ -737,10 +759,12 @@ export default {
             "attributes": {
                 "address": "adres",
                 "age": "leeftijd",
+                "amount": "bedrag",
                 "available": "beschikbaar",
                 "city": "stad",
                 "content": "inhoud",
                 "country": "land",
+                "currency": "valuta",
                 "date": "datum",
                 "day": "dag",
                 "description": "omschrijving",
@@ -783,15 +807,31 @@ export default {
             "profile": "Profiel",
             "account": "Account",
             "preferences": "Voorkeuren",
+            "themes": {
+                "light": "Licht",
+                "dark": "Donker (Experimenteel)"
+            }
+        },
+        "messages": {
             "empty_state": "Er zijn nog geen {resource}",
             "verify_account": "Je moet je account nog verifiëren — check je e-mail",
+            "email_sent": "Als je je met dat adres hebt ingeschreven, hebben we je een e-mail gestuurd",
+            "password_changed": "Je wachtwoord is gewijzigd",
+            "login_failed": "Inloggen mislukt",
+            "verified_account": "Je account is geverifieerd",
             "spaces_explanation": "Spaces kunnen worden gebruikt om je financiën op sorteren in Budget. Bijvoorbeeld — je kan een space hebben voor jou persoonlijk en een andere voor je bedrijf.",
             "know_how_to_make_this_app_better": "Weet je wat er beter kan?",
             "successfully_deleted": "Je hebt succesvol die {resource} verwijderd",
             "still_able_to_recover": "Je kan hem nog herstellen",
-            "themes": {
-                "light": "Licht",
-                "dark": "Donker (Experimenteel)"
+            "transaction_wizard": {
+                "new_earning_description": "Loonstrook februari",
+                "new_spending_description": "Verjaardagscadeau voor Angela",
+                "recurring_explanation": "Dit is een doorlopende transactie",
+                "recurring_duration": "Hoe lang duurt deze doorlopende transactie?",
+                "forever": "Oneindig :(",
+                "until": "Tot en met",
+                "loading": "Laden",
+                "successfully_created": "Je hebt succesvol die transactie aangemaakt"
             }
         },
         "pages": {
@@ -865,11 +905,114 @@ export default {
             ]
         },
         "validation": {
-            "required": "Não pode estar em branco",
-            "confirmed": "Não corresponde",
+            "accepted": "O campo {attribute} deverá ser aceite.",
+            "active_url": "O campo {attribute} não contém um URL válido.",
+            "after": "O campo {attribute} deverá conter uma data posterior a {date}.",
+            "after_or_equal": "O campo {attribute} deverá conter uma data posterior ou igual a {date}.",
+            "alpha": "O campo {attribute} deverá conter apenas letras.",
+            "alpha_dash": "O campo {attribute} deverá conter apenas letras, números e traços.",
+            "alpha_num": "O campo {attribute} deverá conter apenas letras e números .",
+            "array": "O campo {attribute} deverá conter uma coleção de elementos.",
+            "before": "O campo {attribute} deverá conter uma data anterior a {date}.",
+            "before_or_equal": "O Campo {attribute} deverá conter uma data anterior ou igual a {date}.",
+            "between": {
+                "numeric": "O campo {attribute} deverá ter um valor entre {min} - {max}.",
+                "file": "O campo {attribute} deverá ter um tamanho entre {min} - {max} kilobytes.",
+                "string": "O campo {attribute} deverá conter entre {min} - {max} caracteres.",
+                "array": "O campo {attribute} deverá conter entre {min} - {max} elementos."
+            },
+            "boolean": "O campo {attribute} deverá conter o valor verdadeiro ou falso.",
+            "confirmed": "A confirmação para o campo {attribute} não coincide.",
+            "date": "O campo {attribute} não contém uma data válida.",
+            "date_equals": "The {attribute} must be a date equal to {date}.",
+            "date_format": "A data indicada para o campo {attribute} não respeita o formato {format}.",
+            "different": "Os campos {attribute} e {other} deverão conter valores diferentes.",
+            "digits": "O campo {attribute} deverá conter {digits} caracteres.",
+            "digits_between": "O campo {attribute} deverá conter entre {min} a {max} caracteres.",
+            "dimensions": "O campo {attribute} deverá conter uma dimensão de imagem válida.",
+            "distinct": "O campo {attribute} contém um valor duplicado.",
+            "email": "O campo {attribute} não contém um endereço de correio eletrónico válido.",
+            "exists": "O valor selecionado para o campo {attribute} é inválido.",
+            "file": "O campo {attribute} deverá conter um ficheiro.",
+            "filled": "É obrigatória a indicação de um valor para o campo {attribute}.",
+            "gt": {
+                "numeric": "The {attribute} must be greater than {value}.",
+                "file": "The {attribute} must be greater than {value} kilobytes.",
+                "string": "The {attribute} must be greater than {value} characters.",
+                "array": "The {attribute} must have more than {value} items."
+            },
+            "gte": {
+                "numeric": "The {attribute} must be greater than or equal {value}.",
+                "file": "The {attribute} must be greater than or equal {value} kilobytes.",
+                "string": "The {attribute} must be greater than or equal {value} characters.",
+                "array": "The {attribute} must have {value} items or more."
+            },
+            "image": "O campo {attribute} deverá conter uma imagem.",
+            "in": "O campo {attribute} não contém um valor válido.",
+            "in_array": "O campo {attribute} não existe em {other}.",
+            "integer": "O campo {attribute} deverá conter um número inteiro.",
+            "ip": "O campo {attribute} deverá conter um IP válido.",
+            "ipv4": "O campo {attribute} deverá conter um IPv4 válido.",
+            "ipv6": "O campo {attribute} deverá conter um IPv6 válido.",
+            "json": "O campo {attribute} deverá conter um texto JSON válido.",
+            "lt": {
+                "numeric": "The {attribute} must be less than {value}.",
+                "file": "The {attribute} must be less than {value} kilobytes.",
+                "string": "The {attribute} must be less than {value} characters.",
+                "array": "The {attribute} must have less than {value} items."
+            },
+            "lte": {
+                "numeric": "The {attribute} must be less than or equal {value}.",
+                "file": "The {attribute} must be less than or equal {value} kilobytes.",
+                "string": "The {attribute} must be less than or equal {value} characters.",
+                "array": "The {attribute} must not have more than {value} items."
+            },
             "max": {
-                "string": "Não pode ser maior que os caracteres máximos"
-            }
+                "numeric": "O campo {attribute} não deverá conter um valor superior a {max}.",
+                "file": "O campo {attribute} não deverá ter um tamanho superior a {max} kilobytes.",
+                "string": "O campo {attribute} não deverá conter mais de {max} caracteres.",
+                "array": "O campo {attribute} não deverá conter mais de {max} elementos."
+            },
+            "mimes": "O campo {attribute} deverá conter um ficheiro do tipo: {values}.",
+            "mimetypes": "O campo {attribute} deverá conter um ficheiro do tipo: {values}.",
+            "min": {
+                "numeric": "O campo {attribute} deverá ter um valor superior ou igual a {min}.",
+                "file": "O campo {attribute} deverá ter no mínimo {min} kilobytes.",
+                "string": "O campo {attribute} deverá conter no mínimo {min} caracteres.",
+                "array": "O campo {attribute} deverá conter no mínimo {min} elementos."
+            },
+            "not_in": "O campo {attribute} contém um valor inválido.",
+            "not_regex": "The {attribute} format is invalid.",
+            "numeric": "O campo {attribute} deverá conter um valor numérico.",
+            "present": "O campo {attribute} deverá estar presente.",
+            "regex": "O formato do valor para o campo {attribute} é inválido.",
+            "required": "É obrigatória a indicação de um valor para o campo {attribute}.",
+            "required_if": "É obrigatória a indicação de um valor para o campo {attribute} quando o valor do campo {other} é igual a {value}.",
+            "required_unless": "É obrigatória a indicação de um valor para o campo {attribute} a menos que {other} esteja presente em {values}.",
+            "required_with": "É obrigatória a indicação de um valor para o campo {attribute} quando {values} está presente.",
+            "required_with_all": "É obrigatória a indicação de um valor para o campo {attribute} quando um dos {values} está presente.",
+            "required_without": "É obrigatória a indicação de um valor para o campo {attribute} quando {values} não está presente.",
+            "required_without_all": "É obrigatória a indicação de um valor para o campo {attribute} quando nenhum dos {values} está presente.",
+            "same": "Os campos {attribute} e {other} deverão conter valores iguais.",
+            "size": {
+                "numeric": "O campo {attribute} deverá conter o valor {size}.",
+                "file": "O campo {attribute} deverá ter o tamanho de {size} kilobytes.",
+                "string": "O campo {attribute} deverá conter {size} caracteres.",
+                "array": "O campo {attribute} deverá conter {size} elementos."
+            },
+            "starts_with": "The {attribute} must start with one of the following: {values}",
+            "string": "O campo {attribute} deverá conter texto.",
+            "timezone": "O campo {attribute} deverá ter um fuso horário válido.",
+            "unique": "O valor indicado para o campo {attribute} já se encontra registado.",
+            "uploaded": "O upload do ficheiro {attribute} falhou.",
+            "url": "O formato do URL indicado para o campo {attribute} é inválido.",
+            "uuid": "The {attribute} must be a valid UUID.",
+            "custom": {
+                "attribute-name": {
+                    "rule-name": "custom-message"
+                }
+            },
+            "attributes": []
         },
         "general": {
             "dashboard": "Painel de Controle",
