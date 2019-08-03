69b084b2e8ae38f163cc0e32a075ce50ed98414d
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-03 19:56:18 +0200

    Revert "Bugfix"
    This reverts commit f64be0548485d7622326cfc1b89990ff7670077b.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth;
+use Auth;
 
 class IndexController extends Controller {
     public function index() {
