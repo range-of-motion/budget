c70b2033d41cadce42a39c4a333592e7639fbe7a
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-04 13:41:32 +0200

    Added styling for the alerts

--- resources/views/partials/alerts/danger.blade.php
+++ resources/views/partials/alerts/danger.blade.php
@@ -1,13 +1,8 @@
-<div class="row {{ $payload['classes'] }}" style="
-    padding: 15px;
-    color: #FFF;
-    background: #F86380;
-    border-radius: 5px;
-">
-    <div class="row__column row__column--compact" style="margin: auto; width: 5%;">
+<div class="row alert alert-danger {{ $payload['classes'] }}">
+    <div class="row__column row__column--compact alert-fa" >
         <i class="fas fa-times fa-fw"></i>
     </div>
-    <div class="row__column text-center" style="font-weight: 600;">
+    <div class="row__column text-center alert-message">
         {!! __('messages.' . $payload['message']) !!}
         @if($payload['message'] === 'login_failed.verify_account')
             <a class="alert-a" href="{{ route('resend_verify_registration') }}"> {{ __('messages.resend_verify_registration') }}</a>
--- resources/views/partials/alerts/success.blade.php
+++ resources/views/partials/alerts/success.blade.php
@@ -1,13 +1,8 @@
-<div class="row {{ $payload['classes'] }}" style="
-    padding: 15px;
-    color: #FFF;
-    background: #AAD26F;
-    border-radius: 5px;
-">
-    <div class="row__column row__column--compact" style="margin: auto; width: 5%;">
+<div class="row alert alert-success {{ $payload['classes'] }}">
+    <div class="row__column row__column--compact alert-fa">
         <i class="fas fa-check fa-fw"></i>
     </div>
-    <div class="row__column text-center" style="font-weight: 600;">
+    <div class="row__column text-center alert-message">
         {!! __('messages.' . $payload['message']) !!}
     </div>
 </div>
