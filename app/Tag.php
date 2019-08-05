135bfbec1d1eaed933724754abe4cbb5cb8a5ea9
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-05 22:10:09 +0200

    Get all earnings for tag

--- app/Tag.php
+++ app/Tag.php
@@ -24,6 +24,10 @@ class Tag extends Model {
         return $this->hasMany(Spending::class);
     }
 
+    public function earnings() {
+        return $this->hasMany(Earning::class);
+    }
+
     // Custom
     private static function randomColorPart() {
         return str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
