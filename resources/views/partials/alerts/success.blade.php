f884c138264bc55157da47742f053c2c177fd69a
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-06 22:27:35 +0200

    Added scss styling for partials

--- resources/views/partials/alerts/success.blade.php
+++ resources/views/partials/alerts/success.blade.php
@@ -1,11 +1,6 @@
-<div class="row {{ $payload['classes'] }}" style="
-    padding: 15px;
-    color: #FFF;
-    background: #AAD26F;
-    border-radius: 5px;
-">
-    <div class="row__column row__column--compact">
-        <i class="fas fa-check"></i>
+<div class="row alert alert-success {{ $payload['classes'] }}">
+    <div class="row__column row__column--compact alert-fa">
+        <i class="fas fa-check fa-fw"></i>
     </div>
-    <div class="row__column text-center" style="font-weight: 600;">{{ $payload['message'] }}</div>
+    <div class="row__column text-center alert-message">{{ $payload['message'] }}</div>
 </div>
