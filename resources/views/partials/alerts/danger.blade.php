17ece2124b976a7e36b39a64b41e7b1874d1ea47
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-07 07:44:34 +0200

    Add scss styling for partials

--- resources/views/partials/alerts/danger.blade.php
+++ resources/views/partials/alerts/danger.blade.php
@@ -1,11 +1,6 @@
-<div class="row {{ $payload['classes'] }}" style="
-    padding: 15px;
-    color: #FFF;
-    background: #F86380;
-    border-radius: 5px;
-">
-    <div class="row__column row__column--compact">
-        <i class="fas fa-times"></i>
+<div class="row alert alert-danger {{ $payload['classes'] }}">
+    <div class="row__column row__column--compact alert-fa" >
+        <i class="fas fa-times fa-fw"></i>
     </div>
-    <div class="row__column text-center" style="font-weight: 600;">{{ $payload['message'] }}</div>
+    <div class="row__column text-center alert-message">{{ $payload['message'] }}</div>
 </div>
