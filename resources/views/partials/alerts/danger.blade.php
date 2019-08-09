47fc31eea60da97ecac07df6a914e4bd2bfae116
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-09 23:35:21 +0200

    Modifier-naming convention

--- resources/views/partials/alerts/danger.blade.php
+++ resources/views/partials/alerts/danger.blade.php
@@ -1,6 +1,6 @@
-<div class="row alert alert-danger {{ $payload['classes'] }}">
-    <div class="row__column row__column--compact alert-fa" >
+<div class="row alert alert--danger {{ $payload['classes'] }}">
+    <div class="row__column row__column--compact alert--fa" >
         <i class="fas fa-times fa-fw"></i>
     </div>
-    <div class="row__column text-center alert-message">{{ $payload['message'] }}</div>
+    <div class="row__column text-center alert--message">{{ $payload['message'] }}</div>
 </div>
