0eec3d9b075f55ab869ca0e976f3789773e2d980
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-09 23:44:13 +0200

    Remove action from empty-state because all pages have a button for the action

--- resources/views/partials/empty_state.blade.php
+++ resources/views/partials/empty_state.blade.php
@@ -1,4 +1,3 @@
 <div class="box__section text-center">
-    <div class="mb-1">{{ __('general.empty_state', ['resource' => strtolower(__('models.' . $payload))]) }}</div>
-    <a href="/{{ $payload }}/create" style="font-size: 14px;">{{ __('actions.create') }}</a>
+    <div class="mb-1">{{ __('messages.empty_state', ['resource' => strtolower(__('models.' . $payload))]) }}</div>
 </div>
