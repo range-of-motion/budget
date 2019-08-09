3c4fb496a54d2d9b0fe22081fd36513767f1cf4c
Sebastiaan Speck <sebastiaanspeck@users.noreply.github.com>
2019-08-10 00:06:40 +0200

    Make subcomponent for fa and a

--- resources/views/partials/alerts/success.blade.php
+++ resources/views/partials/alerts/success.blade.php
@@ -1,5 +1,5 @@
 <div class="row alert alert--success {{ $payload['classes'] }}">
-    <div class="row__column row__column--compact alert--fa">
+    <div class="row__column row__column--compact alert__fa">
         <i class="fas fa-check fa-fw"></i>
     </div>
     <div class="row__column text-center alert__message">{{ $payload['message'] }}</div>
