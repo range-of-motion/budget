80170821e3ea75f0b53d5ed71342acd58534d6a7
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-24 21:00:36 +0200

    Rework based on comments on PR

--- resources/views/partials/alerts/danger.blade.php
+++ resources/views/partials/alerts/danger.blade.php
@@ -1,6 +1,6 @@
-<div class="row alert alert--danger {{ $payload['classes'] }}">
-    <div class="row__column row__column--compact alert__fa" >
+<div class="alert alert--danger {{ $payload['classes'] }}">
+    <div class="alert__fa" >
         <i class="fas fa-times fa-fw"></i>
     </div>
-    <div class="row__column text-center alert__message">{{ $payload['message'] }}</div>
+    <div class="alert__message">{{ $payload['message'] }}</div>
 </div>
--- resources/views/partials/alerts/success.blade.php
+++ resources/views/partials/alerts/success.blade.php
@@ -1,6 +1,6 @@
-<div class="row alert alert--success {{ $payload['classes'] }}">
-    <div class="row__column row__column--compact alert__fa">
+<div class="alert--success {{ $payload['classes'] }}">
+    <div class="alert__fa">
         <i class="fas fa-check fa-fw"></i>
     </div>
-    <div class="row__column text-center alert__message">{{ $payload['message'] }}</div>
+    <div class="alert__message">{{ $payload['message'] }}</div>
 </div>
