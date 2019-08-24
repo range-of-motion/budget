1d5a37b3ee0663784ac7026f9e77f3ef69816fa1
Sebastiaan Speck <shem.speck@gmail.com>
2019-08-24 20:34:53 +0200

    Set the locale for Carbon, so diffForHumans is translated

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Carbon\Carbon;
 
 class SetLocale {
     public function handle($request, Closure $next) {
@@ -10,6 +11,7 @@ class SetLocale {
             $user = \Auth::user();
 
             \App::setLocale($user->language);
+            Carbon::setLocale($user->language);
         }
 
         return $next($request);
