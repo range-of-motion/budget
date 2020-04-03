c4f2844bd5a4c14792878b23c667896945db9a44
Daniël van Drunen <daniel@pixely.me>
2020-04-03 16:34:04 +0200

    Upgrade Laravel from 5.6 to 5.7 and Carbon from 1.25 to 1.39

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,8 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
