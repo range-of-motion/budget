78f534b0c3c3a3b75e743e676c6b29c4a38e1291
Maxence Brochier <39099689+Maxoulfou@users.noreply.github.com>
2020-04-08 14:18:20 +0200

    Update error
    I fix that bug:
    Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
 
 use Auth;
 
 class AppServiceProvider extends ServiceProvider {
     public function boot() {
+        Builder::defaultStringLength(191);
+        
         view()->composer('*', function($view) {
             $view->with([
                 'userName' => Auth::check() ? Auth::user()->name : null,
