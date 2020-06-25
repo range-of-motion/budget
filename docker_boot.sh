c55477ab0f5f6e4c6dfdbefe061cbf44b1619fc0
Sascha Hemleb <github@sascha.hemleb.de>
2020-06-25 23:36:32 +0200

    Force migrate so it runs in APP_ENV=production

--- docker_boot.sh
+++ docker_boot.sh
@@ -5,7 +5,7 @@ set -e
 DB_HOST="${DB_HOST:?environment variable is missing}"
 DB_PORT="${DB_PORT:-3306}"
 
-./wait-for-it.sh "${DB_HOST}:${DB_PORT}" -- php artisan migrate
+./wait-for-it.sh "${DB_HOST}:${DB_PORT}" -- php artisan migrate --force
 
 # php artisan optimize
 
