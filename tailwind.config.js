a7864ccab0d46d631388be477fb2873fe9e1184a
Daniël van Drunen <daniel@pixely.me>
2020-12-02 22:39:15 +0100

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'tw-',
+}
