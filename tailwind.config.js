48c8defb479ace742992953a0573958618227a63
Daniël van Drunen <daniel@pixely.me>
2020-12-24 02:25:28 +0100

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  prefix: 'tw-',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 let mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 mix
     .sass('resources/assets/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind.config.js') ],
+    })
     .js('resources/assets/js/app.js', 'public/js');
