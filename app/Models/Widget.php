06c92cfc342199dab4907203a6bf4c178b7b7eb2
Daniël van Drunen <daniel@pixely.me>
2021-01-04 14:10:24 +0100

    Test Widget model and dashboard itself

--- app/Models/Widget.php
+++ app/Models/Widget.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use App\Exceptions\WidgetInvalidPropertyValueException;
 use App\Exceptions\WidgetMissingPropertyException;
 use App\Exceptions\WidgetUnknownTypeException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Widget extends Model
 {
+    use HasFactory;
     use SoftDeletes;
 
     protected $fillable = [
---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Space;
+use App\Models\User;
+use App\Models\Widget;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DashboardTest extends TestCase
+{
+    public function testZeroWidgets(): void
+    {
+        $user = User::factory()->create();
+        $space = Space::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->withSession(['space_id' => $space->id])
+            ->get('/dashboard');
+
+        $response->assertStatus(200);
+    }
+
+    public function testBalanceWidget(): void
+    {
+        $user = User::factory()->create();
+        $space = Space::factory()->create();
+
+        Widget::factory()->create([
+            'user_id' => $user->id,
+            'type' => 'balance'
+        ]);
+
+        $response = $this
+            ->actingAs($user)
+            ->withSession(['space_id' => $space->id])
+            ->get('/dashboard');
+
+        $response
+            ->assertStatus(200)
+            ->assertSeeText('Balance');
+    }
+
+    public function testSpentWidget(): void
+    {
+        $user = User::factory()->create();
+        $space = Space::factory()->create();
+
+        Widget::factory()->create([
+            'user_id' => $user->id,
+            'type' => 'spent',
+            'properties' => ['period' => 'this_month']
+        ]);
+
+        $response = $this
+            ->actingAs($user)
+            ->withSession(['space_id' => $space->id])
+            ->get('/dashboard');
+
+        $response
+            ->assertStatus(200)
+            ->assertSeeText('Spent this month');
+    }
+}
