10620ca0413710cc6445c52688df6499c2f5a13c
Daniël van Drunen <daniel@pixely.me>
2021-04-14 11:57:53 +0200

    Upgrade to Laravel Mix 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ let mix = require('laravel-mix');
 
 mix
     .sass('resources/assets/sass/app.scss', 'public/css')
-    .js('resources/assets/js/app.js', 'public/js');
+    .js('resources/assets/js/app.js', 'public/js').vue();
