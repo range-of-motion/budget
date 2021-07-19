9770abd10476f2a6c160b0f9b09504b451911097
whwlsfb <whwlsfb@wanghw.cn>
2021-07-19 17:12:12 +0800

    add simplified Chinese support.

---
+++ resources/lang/cn/reports.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'weekly_balance' => [
+        'title' => '每周余额',
+        'description' => '图形化展示每周的余额情况。'
+    ],
+
+    'most_expensive_tags' => [
+        'title' => '支出金额最大的标签',
+        'description' => '显示所有交易记录中，支出金额最大的标签。'
+    ]
+];
