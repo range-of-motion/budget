1aeb89434db6dbfa0898ddea8a5a4e902c2bb7f8
Daniël van Drunen <daniel@pixely.me>
2022-02-25 20:03:27 +0100

    Set up Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@ let mix = require('laravel-mix');
 
 mix
     .sass('resources/assets/sass/app.scss', 'public/css')
+    .postCss('resources/assets/css/tailwind.css', 'public/css', [
+        require("tailwindcss"),
+    ])
     .js('resources/assets/js/app.js', 'public/js').vue();
