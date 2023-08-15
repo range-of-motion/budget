efde59fd0a46c39126ee48f0e615e7622021155c
Daniël van Drunen <daniel@pixely.me>
2023-08-16 01:05:36 +0200

    Prepare for Docker Hub release

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+appKey=$(awk -F= '$1 == "APP_KEY" {print $2}' .env)
+
+if [ -z "$appKey" ]; then
+  php artisan key:generate
+fi
+
+php artisan config:cache
+
+php artisan migrate --force
+
+supervisord -n -c supervisord.conf
+
+exec "$@"
