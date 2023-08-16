20f1c09eb95bbab0da233fb7820bc4ea5a485075
Daniël van Drunen <daniel@pixely.me>
2023-08-16 19:54:24 +0200

    Create command for updating version file

---
+++ app/Console/Commands/UpdateVersionFile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateVersionFile extends Command
+{
+    protected $signature = 'app:update-version-file';
+
+    protected $description = 'Updates the version in version.txt';
+
+    public function handle(): int
+    {
+        if (trim(shell_exec('git rev-parse --is-inside-work-tree')) !== 'true') {
+            $this->warn('Not inside a Git repository, skipping');
+
+            return 0;
+        }
+
+        $version = trim(shell_exec('git describe --always --tags'));
+
+        $versionFile = fopen('version.txt', 'w');
+
+        fwrite($versionFile, $version);
+
+        fclose($versionFile);
+
+        $this->info('Detected ' . $version . ' and wrote to version file');
+
+        return 0;
+    }
+}
