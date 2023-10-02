16d3052eb8ebe4099736cc9fa8846a364a9f370a
Zander van der Meer <zander.meer@gmail.com>
2023-10-02 21:41:42 +0200

    Moved docker files to separate directory and modified it so it also works for local development using docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+su - www-data << 'EOF'
+    composer install --no-dev --no-interaction
+
+    npm install
+    npm run build
+
+    cp .env.docker .env
+
+    php artisan storage:link
+EOF
+
 appKey=$(awk -F= '$1 == "APP_KEY" {print $2}' .env)
 
 if [ -z "$appKey" ]; then
@@ -10,6 +21,6 @@ php artisan config:cache
 
 php artisan migrate --force
 
-supervisord -n -c supervisord.conf
+supervisord -n -c docker/supervisord.conf
 
 exec "$@"
