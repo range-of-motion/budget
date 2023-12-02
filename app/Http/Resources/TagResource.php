e9b2d90b31dcd4ab37a5fbe0197c126f9f9c8636
Daniël van Drunen <daniel@pixely.me>
2023-12-02 20:39:00 +0100

    Show tags alongside transactions in SPA prototype

---
+++ app/Http/Resources/TagResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'color' => $this->color,
+        ];
+    }
+}
