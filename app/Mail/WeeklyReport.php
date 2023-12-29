b50fea3b891f827908339cc328018da86dad0d41
Daniël van Drunen <daniel@pixely.me>
2023-12-29 22:24:51 +0100

    Test WeeklyReport mail

--- app/Mail/WeeklyReport.php
+++ app/Mail/WeeklyReport.php
@@ -12,17 +12,13 @@ class WeeklyReport extends Mailable
     use Queueable;
     use SerializesModels;
 
-    protected $space;
-    protected $week;
-    protected $totalSpent;
-    protected $largestSpendingWithTag;
-
-    public function __construct(Space $space, $week, $totalSpent, $largestSpendingWithTag)
-    {
-        $this->space = $space;
-        $this->week = $week;
-        $this->totalSpent = $totalSpent;
-        $this->largestSpendingWithTag = $largestSpendingWithTag;
+    public function __construct(
+        protected Space $space,
+        protected $week,
+        protected $totalSpent,
+        protected $largestSpendingWithTag
+    ) {
+        //
     }
 
     public function build()
