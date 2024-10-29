bd7867a1831fa55d6a5eee77fff4f9cf16a014ff
Daniël van Drunen <daniel@pixely.me>
2024-10-29 11:50:53 +0100

    Show monthly statistics and breakdown on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Earning;
+use App\Models\Spending;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Dashboard');
+        $space = Auth::user()->spaces()->first();
+
+        $yearMonth = now()->format('F Y');
+
+        $earnings = Earning::query()
+            ->where('space_id', $space->id)
+            ->whereBetween('happened_on', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $spendings = Spending::query()
+            ->where('space_id', $space->id)
+            ->whereBetween('happened_on', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $tags = Spending::query()
+            ->where('space_id', $space->id)
+            ->whereNotNull('tag_id')
+            ->whereBetween('happened_on', [now()->startOfMonth(), now()->endOfMonth()])
+            ->get()
+            ->groupBy('tag_id')
+            ->map(fn (Collection $s) => [
+                'name' => $s->first()->tag->name,
+                'amount' => sprintf('%s %s', $space->currency->symbol, number_format($s->sum('amount') / 100, 2)),
+                'percentage' => round(($s->sum('amount') / $spendings) * 100, 1),
+            ])
+            ->sortByDesc('percentage')
+            ->values()
+            ->all();
+
+        return Inertia::render(
+            'Dashboard',
+            [
+                'yearMonth' => $yearMonth,
+                'earnings' => sprintf('%s%s', '$', number_format($earnings / 100, 2)),
+                'spendings' => sprintf('%s%s', '$', number_format($spendings / 100, 2)),
+                'net' => sprintf('%s%s', '$', number_format(($earnings - $spendings) / 100, 2)),
+                'tags' => $tags,
+            ],
+        );
     }
 }
--- resources/assets/js/pages/Dashboard.vue
+++ resources/assets/js/pages/Dashboard.vue
@@ -1,3 +1,53 @@
+<script setup>
+defineProps({
+    yearMonth: String,
+    earnings: String,
+    spendings: String,
+    net: String,
+    tags: Array,
+});
+</script>
+
 <template>
-    <div>Dashboard</div>
+    <div>
+        <div class="my-10 mx-auto max-w-3xl">
+            <div class="mb-5">
+                <div class="mb-2 font-bold text-xl">Dashboard</div>
+                <div class="text-sm text-gray-500" v-html="yearMonth"></div>
+            </div>
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <div class="flex divide-x divide-gray-100">
+                    <div class="flex-1 p-5">
+                        <div class="mb-1 font-semibold text-lg" v-html="earnings"></div>
+                        <div class="text-sm text-gray-500">Earnings</div>
+                    </div>
+                    <div class="flex-1 p-5">
+                        <div class="mb-1 font-semibold text-lg" v-html="spendings"></div>
+                        <div class="text-sm text-gray-500">Spendings</div>
+                    </div>
+                    <div class="flex-1 p-5">
+                        <div class="mb-1 font-semibold text-lg" v-html="net"></div>
+                        <div class="text-sm text-gray-500">Net</div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-10 mb-5">
+                <div class="font-semibold">Breakdown</div>
+            </div>
+            <div class="bg-white border border-gray-200 rounded-lg">
+                <div class="divide-y divide-gray-100">
+                    <div class="p-5 flex items-center space-x-5" v-for="tag in tags">
+                        <div class="flex-1 text-sm">{{ tag.name }}</div>
+                        <div class="flex-1 flex items-center">
+                            <div class="w-72 h-1.5 bg-gray-100 rounded-full">
+                                <div class="min-w-2 h-1.5 bg-gray-900 rounded-full" :style="'width: ' + tag.percentage + '%;'"></div>
+                            </div>
+                            <div class="ml-5 text-sm text-gray-500">{{ tag.percentage }}%</div>
+                        </div>
+                        <div class="flex-1 text-right text-sm" v-html="tag.amount"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
